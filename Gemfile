42ca7b667f1f39ff63e98af44245d253e46f3d6c
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-04-01 08:27:04 +0900

    Add gem for octokit. And display github user client.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in github_actions_billing_visualization.gemspec
 gemspec
 
+gem 'octokit', '~> 4.0'
 gem 'rake', '~> 12.0'
 gem 'rspec', '~> 3.0'
--- lib/github_actions_billing_visualization.rb
+++ lib/github_actions_billing_visualization.rb
@@ -1,9 +1,12 @@
 require 'github_actions_billing_visualization/version'
+require 'octokit'
 
 module GithubActionsBillingVisualization
 
-  def self.greet
-    'Hello'
+  def self.run
+    access_token = ENV['ACCESS_TOKEN']
+    client = Octokit::Client.new(access_token: access_token)
+    puts client.user.inspect
   end
 
   class Error < StandardError; end
