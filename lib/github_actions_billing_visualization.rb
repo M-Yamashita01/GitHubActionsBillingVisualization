90e9cf89d9690646753ac1838a5ae4c2d87fa628
M.Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-04-05 14:15:56 +0900

    Get workflow usage by api.

--- lib/github_actions_billing_visualization.rb
+++ lib/github_actions_billing_visualization.rb
@@ -6,7 +6,13 @@ module GithubActionsBillingVisualization
   def self.run
     access_token = ENV['ACCESS_TOKEN']
     client = Octokit::Client.new(access_token: access_token)
-    puts client.user.inspect
+    repo = {
+      owner: 'M-Yamashita01',
+      repo: 'GitHubActionsBillingVisualization'
+    }
+    client.workflow_usage(repo, '')
+
+    # puts client.user.inspect
   end
 
   class Error < StandardError; end
